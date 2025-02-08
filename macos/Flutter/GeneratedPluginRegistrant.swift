276ce6848b27b34bb76f3ecbefb1660136f35b4f
Jeffrey <laventejeffrey@gmail.com>
2025-02-08 17:16:38 +0100

    Fix: Downgrade intl to 0.18.1 to match flutter_localizations requirement

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,17 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import file_saver
-import flutter_localization
 import open_file_mac
 import path_provider_foundation
 import printing
-import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSaverPlugin.register(with: registry.registrar(forPlugin: "FileSaverPlugin"))
-  FlutterLocalizationPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalizationPlugin"))
   OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
-  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
